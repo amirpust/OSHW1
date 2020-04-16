53f0ebe1704bd2b3047b62538832c4a9c7708cc9
amirpust <54210536+amirpust@users.noreply.github.com>
2020-04-16 18:50:46 +0300

    debug

--- BuiltInCommand.cpp
+++ BuiltInCommand.cpp
@@ -54,6 +54,7 @@ KillCommand::KillCommand(string &decrypted, string &originalCommandLine, string 
         BuiltInCommand(decrypted,originalCommandLine,args,size){
     sig = stoi(splitLine[1],nullptr);
     sig *= -1;
+
     jobId = stoi(splitLine[2], nullptr);
 }
 
