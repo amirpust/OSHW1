9828a96c8076e1378e4138a01ee35ca111be215b
jonathanjosef91 <54238904+jonathanjosef91@users.noreply.github.com>
2020-04-24 16:47:21 +0300

    sys call exception mid work

--- Signals.cpp
+++ Signals.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 void ctrlZHandler(int sig_num) {
     cout << "smash: got ctrl-Z" << endl;
+
+    if(getpid() == SmallShell::getInstance().getMyPid())
+        return;
+
     JobsList::getInstance().sendSigById(SIGSTOP);
     cout <<  "smash: process " << JobsList::getInstance().fgPid() <<
     " was stopped"<< endl;
@@ -11,6 +15,10 @@ void ctrlZHandler(int sig_num) {
 
 void ctrlCHandler(int sig_num) {
     cout<< "smash: got ctrl-C"<< endl;
+
+    if(getpid() == SmallShell::getInstance().getMyPid())
+        return;
+
     string pidStr = std::to_string(JobsList::getInstance().fgPid());
     JobsList::getInstance().sendSigById(sig_num);
     cout<< "smash: process " << pidStr << " was killed"<< endl;
